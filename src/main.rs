1bd5444196f1989bad430fe021a50a3b27819312
Nearlynotguy <nvilbrandt@gmail.com>
2025-02-19 18:20:53 -0800

    Refactored tests, main, lib; Added clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
+use clap::Parser;
 use std::env;
-use ynab_gas_scraper::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     run(&args);
+}
+
+fn run<T>(args: T) {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn run_with_no_input() {
+        let empty_test_input: &[String] = &[];
+        run(empty_test_input);
+    }
+
+    #[test]
+    fn run_with_arbitrary_input() {
+        let test_input = 0;
+        run(test_input);
+    }
+
+    #[test]
+    fn run_with_string() {
+        let test_input = &"hello".to_string();
+        run(test_input);
+    }
 }
